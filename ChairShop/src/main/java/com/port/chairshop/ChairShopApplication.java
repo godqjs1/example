d06cd7b4fd263de99deaf510626745a6cfeed316
godqjs1 <godqjs1@gmail.com>
2024-03-20 15:39:23 +0900

    깃허브 연습

---
+++ ChairShop/src/main/java/com/port/chairshop/ChairShopApplication.java
@@ -0,0 +1,13 @@
+package com.port.chairshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChairShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChairShopApplication.class, args);
+	}
+
+}
---
+++ ChairShop/src/test/java/com/port/chairshop/ChairShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.port.chairshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChairShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
